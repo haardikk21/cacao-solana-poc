913e5c5b639590f7da6c1238c191c63eae548057
Haardik <hhaardik@uwaterloo.ca>
2022-07-06 08:25:57 +0530

    wip: solana poc

---
+++ utils/getProvider.ts
@@ -0,0 +1,22 @@
+import { PhantomProvider } from "../types";
+
+/**
+ * Retrieves the Phantom Provider from the window object
+ * @returns {PhantomProvider | undefined} a Phantom provider if one exists in the window
+ */
+const getProvider = (): PhantomProvider | undefined => {
+  if ("solana" in window) {
+    const anyWindow: any = window;
+    const provider = anyWindow.solana;
+
+    if (provider.isPhantom) {
+      return provider;
+    }
+
+    console.log(provider);
+  }
+
+  window.open("https://phantom.app/", "_blank");
+};
+
+export default getProvider;
